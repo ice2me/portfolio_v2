46ecff027c59b03959130d6d61640c2bd5464dcf
ice2me <ice2me1989@gmail.com>
2022-06-23 16:55:38 +0300

    update

--- src/Main/Projects/Projects.js
+++ src/Main/Projects/Projects.js
@@ -7,49 +7,16 @@ class Projects extends Component {
 		super(props)
 		this.state = {
 			name: {
-				'one' : 'Golden Lion',
-				'two' : 'Avenue Fashion',
-				'three' : 'Avenue Fashion LocalStores',
-				'four' : 'Avenue Fashion LookBook',
-				'five' : 'Avenue Fashion Brands',
-				'six' : 'Magazine',
-				'seven' : 'Wooden',
-				'eight' : 'Squro',
-				'nine' : 'LifeCell',
-				'ten' : 'Business',
-				'eleven' : 'Taxi',
-				'twelve' : 'Foxtrot',
-				'thirteen' : 'FatBike',
+				'one' : 'Recipes by ingredients',
+				'two' : 'ZoomTender',
 			},
 			title: {
-				'one' : 'My site design for the provision of front end development services',
-				'two' : 'An Interactive panoramic HTML5 Catalogue online store main',
-				'three' : 'An Interactive panoramic HTML5 Catalogue online store location in three cities',
-				'four' : 'An Interactive panoramic HTML5 Catalogue online store lookbook',
-				'five' : 'An Interactive panoramic HTML5 Catalogue online store brand history',
-				'six' : 'Online street-style clothing and accessories store',
-				'seven' : 'Progressively Enhanced website experiance, both on Desktop and Mobile for an Wooden brand',
-				'eight' : 'Landing Page brand scuro selling scooters',
-				'nine' : 'mobile operator banner for green office from Ukraine',
-				'ten' : 'A responsive, feature-rich portfolio website for a technology consulting company.',
-				'eleven' : 'A responsive, feature-rich website for a technology taxi company.',
-				'twelve' : 'shop banner for Foxtrot from Ukraine',
-				'thirteen' : 'An Interactive panoramic HTML5 Catalogue online store main',
+				'one' : 'This app was designed and developed to search for recipes by ingredients',
+				'two' : 'This app create tender and allows you to find the contractors'
 			},
 			link: {
-				'one' : 'http://goldenlion.serhii-zuiev.com.ua/en/index__en.html',
-				'two' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/AvenueFashion/index.html',
-				'three' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/AvenueFashion/LocalStores.html',
-				'four' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/AvenueFashion/LookBook.html',
-				'five' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/AvenueFashion/Brands.html',
-				'six' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/Magazine/index.html',
-				'seven' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/Wooden/index.html',
-				'eight' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/Squro/index.html',
-				'nine' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/LifeCell/index.html',
-				'ten' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/WerstkaWebsiteLanding/index.html',
-				'eleven' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/TAXI/indexKiev.html',
-				'twelve' :'http://goldenlion.serhii-zuiev.com.ua/portfolio/Foxtrot/index.html',
-				'thirteen' : 'http://goldenlion.serhii-zuiev.com.ua/portfolio/FatBike/index.html',
+				'one' : 'https://selection-recipe.herokuapp.com/',
+				'two' : 'http://134.209.157.41/',
 			}
 		}
 	}
@@ -66,61 +33,6 @@ class Projects extends Component {
 					name={this.state.name.two}
 					title={this.state.title.two}
 					link={this.state.link.two}
-				/>
-				<ProjectContainer
-					name={this.state.name.three}
-					title={this.state.title.three}
-					link={this.state.link.three}
-				/>
-				<ProjectContainer
-					name={this.state.name.four}
-					title={this.state.title.four}
-					link={this.state.link.four}
-				/>
-				<ProjectContainer
-					name={this.state.name.five}
-					title={this.state.title.five}
-					link={this.state.link.five}
-				/>
-				<ProjectContainer
-					name={this.state.name.six}
-					title={this.state.title.six}
-					link={this.state.link.six}
-				/>
-				<ProjectContainer
-					name={this.state.name.seven}
-					title={this.state.title.seven}
-					link={this.state.link.seven}
-				/>
-				<ProjectContainer
-					name={this.state.name.eight}
-					title={this.state.title.eight}
-					link={this.state.link.eight}
-				/>
-				<ProjectContainer
-					name={this.state.name.nine}
-					title={this.state.title.nine}
-					link={this.state.link.nine}
-				/>
-				<ProjectContainer
-					name={this.state.name.ten}
-					title={this.state.title.ten}
-					link={this.state.link.ten}
-				/>
-				<ProjectContainer
-					name={this.state.name.eleven}
-					title={this.state.title.eleven}
-					link={this.state.link.eleven}
-				/>
-				<ProjectContainer
-					name={this.state.name.twelve}
-					title={this.state.title.twelve}
-					link={this.state.link.twelve}
-				/>
-				<ProjectContainer
-					name={this.state.name.thirteen}
-					title={this.state.title.thirteen}
-					link={this.state.link.thirteen}
 				/>
 				<p>{'<projects>'}</p>
 			</div>
