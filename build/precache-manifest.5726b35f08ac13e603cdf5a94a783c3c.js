9fa1fc151626fb3d7dbada4e3ffdce975a7b0e04
ice2me <ice2me1989@gmail.com>
2022-06-23 17:38:07 +0300

    11

---
+++ build/precache-manifest.5726b35f08ac13e603cdf5a94a783c3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8abd8ef2f5393f93af71ab13a891712",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb77eb749753f93249fc",
+    "url": "/static/css/main.59c3052b.chunk.css"
+  },
+  {
+    "revision": "206d6301c62a7a023ea8",
+    "url": "/static/js/2.5d59821e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d59821e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb77eb749753f93249fc",
+    "url": "/static/js/main.8b5e9dda.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "d97862fdd2994ba442ad189823c638e5",
+    "url": "/static/media/Serhii__Zuiev.d97862fd.pdf"
+  }
+]);
